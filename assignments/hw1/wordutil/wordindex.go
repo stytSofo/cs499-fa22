5d046885c33c2690c0cd332230088e6e915904ad
Stylianos Sofokleous <ssofok02@ucy.ac.cy>
2022-09-20 18:36:47 +0300

    Ex.2 completed

--- assignments/hw1/wordutil/wordindex.go
+++ assignments/hw1/wordutil/wordindex.go
@@ -14,4 +14,19 @@ import (
 func WordIndex(s string) map[string]int {
 	// TODO: implement me
 	// HINT: You may find the `strings.Index` and `strings.ToLower` functions helpful
+
+	var s_split = strings.Fields(s)
+
+	IndexMap := make(map[string]int)
+
+	for _, substring := range s_split {
+
+		if IndexMap[substring] == 0 && substring == strings.ToLower(substring) {
+			IndexMap[substring] = strings.Index(s, substring)
+		}
+
+	}
+
+	return IndexMap
+
 }
--- assignments/hw1/wordutil/wordindexall.go
+++ assignments/hw1/wordutil/wordindexall.go
@@ -2,6 +2,8 @@ package wordutil
 
 import (
 	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 // Finds all occurrences of each word in a string.
@@ -14,4 +16,19 @@ import (
 func WordIndexAll(s string) map[string][]int {
 	// TODO: implement me
 	// HINT: You may find the `strings.Index` and `strings.ToLower` functions helpful
+
+	var s_split = strings.Fields(s)
+
+	IndexMap := make(map[string][]int)
+
+	for _, substring := range s_split {
+
+		if IndexMap[substring] == 0 && substring == strings.ToLower(substring) {
+			IndexMap[substring] = append(IndexMap[substring], strings.Index(s, substring)) 
+		}
+		
+	}
+
+	return IndexMap
+
 }
