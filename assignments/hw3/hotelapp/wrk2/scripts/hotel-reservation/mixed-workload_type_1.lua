5e68772fee0e7725ac9148f32374be786086b700
ZoePassiadou <zpassi01@ucy.ac.cy>
2022-11-16 07:34:07 -0700

    Fixed workload script

--- assignments/hw3/hotelapp/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
+++ assignments/hw3/hotelapp/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
@@ -109,19 +109,7 @@ end
 
 request = function()
   cur_time = math.floor(socket.gettime())
-  local search_ratio      = 0.6
-  local recommend_ratio   = 0.39
-  local user_ratio        = 0.005
-  local reserve_ratio     = 0.005
 
-  local coin = math.random()
-  if coin < search_ratio then
-    return search_hotel(url)
-  elseif coin < search_ratio + recommend_ratio then
-    return recommend(url)
-  elseif coin < search_ratio + recommend_ratio + user_ratio then
-    return user_login(url)
-  else 
-    return reserve(url)
-  end
+  search_hotel()
+  
 end
