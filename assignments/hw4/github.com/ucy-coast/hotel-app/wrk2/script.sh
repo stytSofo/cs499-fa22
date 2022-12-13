1f131d935e3638032b0c6519d6ed5286fa691e6e
Stylianos Sofokleous <ssofok02@ucy.ac.cy>
2022-12-13 15:15:33 +0200

    new copy

---
+++ assignments/hw4/github.com/ucy-coast/hotel-app/wrk2/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for conn in 100 200 300 400 500 600 700 800 900 1000
+	do
+		./wrk -t10 -c"$conn" -d30s -R2000 -L -s ./scripts/hotel-reservation/mixed-workload_type_1.lua http://node2:8080
+	done
